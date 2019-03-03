20705c4e355cfeec8043ac6b9667babb2cd46cca
Alexa Gibson <alexa.gibson@outlook.com>
2019-03-03 17:05:13 +0000

    completed and tested script for events api

--- events.js
+++ events.js
@@ -1,52 +1,43 @@
 
-/*$(document).ready(function() {
-    const baseURL = "https://www.skiddle.com/api/v1/events/search/?api_key=9771bc9fcbcf9d1f827ffde2ad85d909&latitude=55.8608853&longitude=-4.2509067&radius=3&eventcode=LIVE&order=date&description=1";
-    var weather;
-    var weatherDescription;
-    var weatherIcon;
-    var temperature;
-    var minTemperature;
-    var maxTemperature;
-        
-        $.ajax({
-            type: "GET",
-            url: baseURL,
-            dataType: "jsonp",
-            success: function(data) {
-               //display temperatures in Celsius
-                temperature = Math.round(data.main.temp - 273.15) + '°C';
-				minTemperature = Math.round(data.main.temp_min - 273.15) + '°C';
-				maxTemperature = Math.round(data.main.temp_max - 273.15) + '°C';
-				weatherDescription = data.weather[0].main;
-                weather = {
-                    weatherDescription,
-                    temperature,
-                    minTemperature,
-                    maxTemperature
-                    };
-                
-                $("#weather-info").html(`<p>The current weather is ${weatherDescription}<br> current temperature is ${temperature}, today's high ${maxTemperature}, today's low ${minTemperature} </p>`);
-                console.log(data);
-                }
-            }
-        );
-});*/
+var apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNWEwMzYwNGMtMDQwMy00MDc1LWE0NGItNThjOTIwNjIyODQ5Iiwia2V5X2lkIjoiMWM1MjUxOTItMjQzOC00MDZhLWJiZDgtOTczMDViZDU3Njc2IiwiaWF0IjoxNTUwMzE5Mzg2fQ.OReYaxoTq9PiZC5InfxNWT0pRtXWlTSNog6vQYNMerY';
 
-const baseURL = "https://www.skiddle.com/api/v1/events/search/?api_key=9771bc9fcbcf9d1f827ffde2ad85d909&latitude=55.8608853&longitude=-4.2509067&radius=3&eventcode=LIVE&order=date&description=1";
+function eventsData() {
+	var headers = new Headers();
 
-var xhr = new XMLHttpRequest();
+	headers.set('Authorization', 'Bearer '+apiKey);
+	fetch('https://api.list.co.uk/v1/events?location=glasgow&order=ts', { headers: headers })
+		.then(function(response) { 
+		    return response.json(); 
+		    
+		})
+		
+		.then(function(data) {
+			var ul = document.querySelector('#search-output');
+				
+			ul.innerHTML = '';
+			data.forEach(function(result) {
+			    	
+				var rawTime = result.schedules[0].performances[0].ts;
+				var eventTime = moment(rawTime).format("Do MMMM YYYY, h:mm a");
+				
+				var li = document.createElement('li');
+				
+				eventInfo = document.createElement('div');
+				eventInfo.innerHTML = `<h3 class="event-title">${result.name}</h3>
+				                            <p class="event-details"> @ ${result.schedules[0].place.name} <br>
+				                            This event is on ${eventTime} and is ${result.schedules[0].ticket_summary}. <br>
+				                            Click <a href="https://www.list.co.uk/event/${result.event_id}" target="_blank">here</a> for more info.<p>`;
+				li.appendChild(eventInfo);
+				ul.appendChild(li);
+			});
+	    });
+		    
+	
+		
+		
+}
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-          document.getElementById("#events-info").innerHTML = this.responseText;
-    }
-};
-
-xhr.open("GET", baseURL);
-xhr.send();
-
-
-
+eventsData();
 
 
 
